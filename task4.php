694c7afc71137ce27d98af94215d54d870a5d68f
dibbo54 <djs189426@gmail.com>
2023-09-25 22:12:36 +0600

    php
    php

---
+++ task4.php
@@ -0,0 +1,20 @@
+<?php
+function calculateAndPrintAverageGrades($studentGrades) {
+    foreach ($studentGrades as $student => $grades) {
+        $total = array_sum($grades);
+        $average = $total / count($grades);
+        
+        echo "Student $student's Average Grade: $average\n"." , ";
+    }
+}
+
+// Create a multidimensional array with student grades
+$studentGrades = array(
+    'Student1' => array('Math' => 85, 'English' => 92, 'Science' => 78),
+    'Student2' => array('Math' => 88, 'English' => 95, 'Science' => 90),
+    'Student3' => array('Math' => 76, 'English' => 84, 'Science' => 89)
+);
+
+// Call the function to calculate and print average grades
+calculateAndPrintAverageGrades($studentGrades);
+?>
